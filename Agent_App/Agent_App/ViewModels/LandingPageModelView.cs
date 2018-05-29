83e3526939bc2cc10b3b1cf5e273ca1a93835177
ansumala kumarage <anshuwa@gmail.com>
2018-05-29 13:25:32 +0530

    Landing Page added

---
+++ Agent_App/Agent_App/ViewModels/LandingPageModelView.cs
@@ -0,0 +1,25 @@
+﻿using Agent_App.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Agent_App.ViewModels
+{
+    public class LandingPageModelView
+    {
+        public ICommand ProfileCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    //Application.Current.MainPage = new NavigationPage(new ExampleList());
+
+                    await Application.Current.MainPage.Navigation.PushAsync(new ExampleList());
+                });
+            }
+        }
+    }
+}
