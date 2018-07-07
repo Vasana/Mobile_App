b0932e6006b7a3060c1c5be1168122c4f39487a6
Vasana <jayachamaree@gmail.com>
2018-07-07 14:18:14 +0530

    Branch Contacts Modified

--- Agent_App/Agent_App/Models/BranchContact.cs
+++ Agent_App/Agent_App/Models/BranchContact.cs
@@ -13,5 +13,9 @@ namespace Agent_App.Models
         public string ContactNumber2 { get; set; } = "";
         public string ContactNumber3 { get; set; } = "";
         public string ContactNumber4 { get; set; } = "";
+        public string SMContactNumber { get; set; } = "";
+        public string RSMContactNumber { get; set; } = "";
+        public string BrnAdminName { get; set; } = "";
+        public string BrnAdminContactNo { get; set; } = "";
     }
 }
