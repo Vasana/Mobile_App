be633cccd6a2d52448a348fe9f50d52ee86d3608
Vasana <jayachamaree@gmail.com>
2018-08-08 14:47:38 +0530

    changes sofar

--- Agent_App/Agent_App/Helpers/StringToBoolConverter.cs
+++ Agent_App/Agent_App/Helpers/StringToBoolConverter.cs
@@ -11,7 +11,7 @@ namespace Agent_App.Helpers
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (string)value != "";
+            return (string)value != null && (string)value != "";
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
