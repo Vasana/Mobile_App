de72a887484302786d7206ae177ee7f266ef7f60
Vasana <jayachamaree@gmail.com>
2018-09-01 16:33:45 +0530

    issue in Portfolio fixed

---
+++ Agent_App/Agent_App/Helpers/ValueToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Agent_App.Helpers
+{
+    class ValueToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.ToString().Replace("\\n", "\n").Replace("\\u2022", "\u2022");                
+            }
+            else
+                return "";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
