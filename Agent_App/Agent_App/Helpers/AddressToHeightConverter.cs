0ab03aba483040bc0184af889f610ca212f03ec8
Vasana <jayachamaree@gmail.com>
2018-09-06 16:04:35 +0530

    changes so far

--- Agent_App/Agent_App/Helpers/AddressToHeightConverter.cs
+++ Agent_App/Agent_App/Helpers/AddressToHeightConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
@@ -6,16 +7,24 @@ using Xamarin.Forms;
 
 namespace Agent_App.Helpers
 {
-    public class ValueToStringConverter : IValueConverter
+    public class AddressToHeightConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value != null)
+            // return null if value is not of type List<string>
+            //List<string> myModel = value as List<string>;
+
+            // throws exception if value is not of type List<string>
+
+            List<string> strList = (List<string>)value;
+            if (strList != null && strList.Count > 0)
             {
-                return value.ToString().Replace("\\n", "\n").Replace("\\u2022", "\u2022");                
+                return (30 * strList.Count);
             }
             else
-                return "";
+            {
+                return 0;
+            }
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
