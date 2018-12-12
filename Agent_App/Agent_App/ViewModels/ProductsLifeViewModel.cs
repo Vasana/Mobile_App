ad586bf7eafea255b3e130121086eab01f09671b
ansumala kumarage <anshuwa@gmail.com>
2018-12-12 16:24:03 +0530

    Changes so far

---
+++ Agent_App/Agent_App/ViewModels/ProductsLifeViewModel.cs
@@ -0,0 +1,112 @@
+﻿using Agent_App.Helpers;
+using Agent_App.Models;
+using Agent_App.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Extended;
+
+namespace Agent_App.ViewModels
+{
+    public class ProductsLifeViewModel : INotifyPropertyChanged
+    {
+        private ApiServices _apiServices = new ApiServices();
+        public ObservableCollection<Products> _LifeProductList;
+        public ObservableCollection<Products> LifeproductList
+        {
+            get { return _LifeProductList; }
+            set
+            {
+                _LifeProductList = value;
+                OnPropertyChanged();
+            }
+        }
+        public int _GeneralHeight;
+        public int GeneralHeight
+        {
+            get { return _GeneralHeight; }
+            set
+            {
+                _GeneralHeight = value;
+                OnPropertyChanged();
+            }
+        }
+        public Products _oldLifeProduct;
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                _isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+        private bool _isBusy;
+
+        public ProductsLifeViewModel()
+        {
+            DataLoad();
+           // DownloadPoliciesAsync();
+        }
+        
+
+
+        private async Task DataLoad()
+        {
+
+            var genlist = await getGenerealProductListAsync();
+            foreach (Products item in genlist)
+            {
+                item.shortDesc = item.shortDesc.Replace("\\n", "\n");
+            }
+            GeneralHeight = (genlist.Count * 180) + 40;
+            LifeproductList = new ObservableCollection<Products>(genlist);
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void ShowDetailsGen(Products product)
+        {
+
+            if (_oldLifeProduct == product)
+            {
+                product.IsSelected = !product.IsSelected;
+                UpdateProductsGen(product);
+            }
+            else
+            {
+                if (_oldLifeProduct != null)
+                {
+                    _oldLifeProduct.IsSelected = false;
+                    UpdateProductsGen(_oldLifeProduct);
+                }
+                product.IsSelected = true;
+                UpdateProductsGen(product);
+            }
+            _oldLifeProduct = product;
+        }
+
+        private void UpdateProductsGen(Products product)
+        {
+            var index = LifeproductList.IndexOf(product);
+            LifeproductList.Remove(product);
+            LifeproductList.Insert(index, product);
+        }
+
+        public async Task<IList<Products>> getGenerealProductListAsync()
+        {
+            IsBusy = true;
+            var LifeList = await _apiServices.GetProducts("Life", Settings.AccessToken);
+            IsBusy = false;
+            return LifeList;
+        }
+    }
+}
---
+++ Agent_App/Agent_App/Views/Products/LifeProducts.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Agent_App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agent_App.Views.Products
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LifeProducts : ContentPage
+	{
+		public LifeProducts ()
+		{
+			InitializeComponent ();
+            if (Device.Idiom == TargetIdiom.Phone)
+            {
+                firstRow.Height = secondRow.Height = 0;
+            }
+        }
+
+
+        private void listofCommon_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var vm = BindingContext as ProductsLifeViewModel;
+            var product = e.Item as Agent_App.Models.Products;
+            vm.ShowDetailsGen(product);
+
+            var keyword = txtGenproduct.Text;
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                listofGeneral.ItemsSource = vm.LifeproductList.Where(x => (x.productName.ToLower()).Contains(keyword.ToLower()) || (x.shortDesc.ToLower()).Contains(keyword.ToLower()));
+            }
+        }
+        private void txtGenproduct_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var keyword = txtGenproduct.Text;
+            var vm = BindingContext as ProductsLifeViewModel;
+
+            if (String.IsNullOrEmpty(keyword))
+            {
+                listofGeneral.ItemsSource = vm.LifeproductList;
+
+            }
+            else
+            {
+                listofGeneral.ItemsSource = vm.LifeproductList.Where(x => (x.productName.ToLower()).Contains(keyword.ToLower()) || (x.shortDesc.ToLower()).Contains(keyword.ToLower()));
+            }
+
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            var vm = BindingContext as ProductsLifeViewModel;
+
+            try
+            {
+                Button button = (Button)sender;
+                Grid grid = (Grid)button.Parent;
+                StackLayout listViewItem = (StackLayout)grid.Parent;
+                Label lbl_product = (Label)grid.Children[3];
+                Models.Products pro = vm.LifeproductList.FirstOrDefault(X => X.productID == lbl_product.Text);
+                if (pro != null)
+                {
+                    if (!String.IsNullOrEmpty(pro.documentUrl))
+                    {
+                        String url = pro.documentUrl;
+                        Device.OpenUri(new Uri(url));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                string i = ex.ToString();
+            }
+
+        }
+
+    }
+}
