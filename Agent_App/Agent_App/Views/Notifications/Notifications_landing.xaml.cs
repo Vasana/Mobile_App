7149ba6f61f2a4c8a0351c36923367d341655bdb
Jananan Sellathurai <mrjanuplus@hotmail.com>
2018-12-24 11:16:43 +0530

    update

---
+++ Agent_App/Agent_App/Views/Notifications/Notifications_landing.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agent_App.Views.Notifications
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Notifications_landing : TabbedPage
+    {
+		public Notifications_landing ()
+		{
+			InitializeComponent ();
+            BarBackgroundColor = Color.FromHex("#00adbb");
+            Title = "Notifications";
+        }
+	}
+}
