e7fcb0ecfc2ee9230ccca4461d39995929a3526f
Vasana <jayachamaree@gmail.com>
2018-12-27 17:18:15 +0530

    commit

--- Agent_App/Agent_App/Views/CommonUtilsPage.xaml.cs
+++ Agent_App/Agent_App/Views/CommonUtilsPage.xaml.cs
@@ -34,14 +34,14 @@ namespace Agent_App.Views
                 BtnPromotions.Text = "Product Portfolio";
                 BtnBranches.Text = "Branch Contacts";
                 btnSettings.Text = "Settings";
-                BtnNotifications.Text = "Notifications";
+                //BtnNotifications.Text = "Notifications";
 
-                BtnBranches.Image = btnSettings.Image = BtnPromotions.Image = BtnProfile.Image = BtnNotifications.Image = null;
-                btnSettings.HeightRequest = BtnBranches.HeightRequest = BtnPromotions.HeightRequest = BtnProfile.HeightRequest = BtnNotifications.HeightRequest = 50;
-                btnSettings.WidthRequest = BtnBranches.WidthRequest = BtnPromotions.WidthRequest = BtnProfile.WidthRequest = BtnNotifications.WidthRequest = 200;
-                btnSettings.BackgroundColor = BtnBranches.BackgroundColor = BtnPromotions.BackgroundColor = BtnProfile.BackgroundColor = BtnNotifications.BackgroundColor = Color.SkyBlue;
-                btnSettings.BorderColor = BtnBranches.BorderColor = BtnPromotions.BorderColor = BtnProfile.BorderColor = BtnNotifications.BorderColor = Color.FromHex("#C89400");
-                btnSettings.BorderRadius = BtnBranches.BorderRadius = BtnPromotions.BorderRadius = BtnProfile.BorderRadius = BtnNotifications.BorderRadius = 10;
+                BtnBranches.Image = btnSettings.Image = BtnPromotions.Image = BtnProfile.Image /*= BtnNotifications.Image*/ = null;
+                btnSettings.HeightRequest = BtnBranches.HeightRequest = BtnPromotions.HeightRequest = BtnProfile.HeightRequest /*= BtnNotifications.HeightRequest*/ = 50;
+                btnSettings.WidthRequest = BtnBranches.WidthRequest = BtnPromotions.WidthRequest = BtnProfile.WidthRequest /*= BtnNotifications.WidthRequest*/ = 200;
+                btnSettings.BackgroundColor = BtnBranches.BackgroundColor = BtnPromotions.BackgroundColor = BtnProfile.BackgroundColor /*= BtnNotifications.BackgroundColor*/ = Color.SkyBlue;
+                btnSettings.BorderColor = BtnBranches.BorderColor = BtnPromotions.BorderColor = BtnProfile.BorderColor /*= BtnNotifications.BorderColor*/ = Color.FromHex("#C89400");
+                btnSettings.BorderRadius = BtnBranches.BorderRadius = BtnPromotions.BorderRadius = BtnProfile.BorderRadius /*= BtnNotifications.BorderRadius*/ = 10;
                 
             }
         }
